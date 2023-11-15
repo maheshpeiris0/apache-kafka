9d4b8267942c07f74a62d867bd70cd02befc6d0e
Mahesh Peiris <mahesh_sanjaya@yahoo.com>
2023-11-15 05:07:56 +0000

    updated

--- Kafka_Project_1/test.py
+++ Kafka_Project_1/test.py
@@ -1,5 +1,5 @@
 import os
 
 api_key = os.environ.get("MY_API_KEY")
-
 print(api_key)
+
---
+++ producer.py
@@ -0,0 +1,21 @@
+from confluent_kafka import Producer
+import json
+import os
+
+# Kafka configuration
+config = {
+    'bootstrap.servers': os.environ.get('BOOTSTRAP_SERVERS'),  # Replace with your server
+    'sasl.mechanisms': 'PLAIN',
+    'security.protocol': 'SASL_SSL',
+    'sasl.username': os.environ.get('SASL_USERNAME'),               # Replace with your API key
+    'sasl.password': os.environ.get('SASL_PASSWORD')           # Replace with your API secret
+}
+
+# Create Producer instance
+producer = Producer(config)
+
+# Produce a message
+topic = 'poems'
+message = {'5': 'message from github'}
+producer.produce(topic, json.dumps(message))
+producer.flush()
