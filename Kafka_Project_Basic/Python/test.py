9d7f8f1ae4c74df4ec99c907c6266414132444a0
Mahesh Peiris <mahesh_sanjaya@yahoo.com>
2023-11-17 18:05:27 +0000

    updated

--- Kafka_Project_Basic/Python/test.py
+++ Kafka_Project_Basic/Python/test.py
@@ -5,9 +5,13 @@ import random
 import uuid
 
 def random_data():
-    key =  random.randint(1,6)
-    value = uuid.uuid4().hex
-    data = {'key':key,'value':value
+    emp_id = random.randint(1,1000000)
+    first_name = random.choice(['John','Smith','Linda','Sarah','David','James','Robert','Michael','Maria','Mary','Mari','Anna','Emma','Julia','Julie','Samantha','Sara','Sandra','Sofia','Sophia','Sofie','Sofy','Sofe','Sof','Sara','Sar','Sa','S'])
+    last_name = random.choice(['Brown','White','Black','Green','Yellow'])
+    age = random.randint(20,60)
+    department = random.choice(['IT','HR','Finance','Marketing','Sales','Operations','Procurement'])
+    salary = random.randint(55000,500000)
+    data = {'emp_id':emp_id,'first_name':first_name,'last_name':last_name,'age':age,'department':department,'salary':salary
     }
 
     return json.dumps(data)
