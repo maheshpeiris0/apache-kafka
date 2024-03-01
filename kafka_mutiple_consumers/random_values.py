80e3eed1859c26741c70d122e24a3126f61b84f5
Mahesh Peiris <mahesh_sanjaya@yahoo.com>
2024-03-01 01:34:57 +0000

    updated

--- kafka_mutiple_consumers/random_values.py
+++ kafka_mutiple_consumers/random_values.py
@@ -5,7 +5,7 @@ def generate_message():
     """ Generate the message data with random values. """
     return {
         "event_type": "A",
-        "symbol": random.choice(["PLUG", "AAPL", "TSLA", "MSFT", "AMZN", "GOOGL", "FB", "NVDA", "PYPL", "INTC"]),
+        "symbol": random.choice(["PLUG", "AAPL", "TSLA", "MSFT", "AMZN", "GOOGL", "FB", "NVDA", "PYPL", "INTC", "CSCO", "CMCSA", "PEP", "ADBE", "NFLX", "AVGO", "TXN", "COST", "QCOM", "TMUS"]),
         "volume": random.randint(50, 1500),
         "accumulated_volume": random.randint(1000000, 50000000),
         "official_open_price": round(random.uniform(1, 5), 2),
@@ -32,7 +32,7 @@ def produce_messages_forever():
         print(message)
         
         # Wait for 0.5 seconds before generating the next message
-        time.sleep(0.05)
+        #time.sleep(0.05)
 
 # Call the function to start generating messages
 produce_messages_forever()
